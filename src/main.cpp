46489e443058e77a8f86f4d9c40f61efa022e638
dobri-dobrev <dmd2169@columbia.edu>
2014-05-10 19:45:53 -0400

    some moar stuff

--- src/main.cpp
+++ src/main.cpp
@@ -526,7 +526,9 @@ static void setupShaders()
     shaderProg = toon;
 }
 void moveBall(){
+    GLfloat force_coeff = 
     ball_offset = -verts[TOTAL_I_VERTS/4+ TOTAL_J_VERTS*TOTAL_I_VERTS/2].y;
+
     
 }
 
